5805cbb7a737657e96647e8ac6b6215ed9b41156
timothieecantcode <timothienguyen@gmail.com>
2025-04-07 23:12:59 +0930

    update

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,15 +1,15 @@
 <template>
   <header>
-    <div class="flex justify-between items-center p-5 px-10 relative z-20">
-      <div class="text-3xl md:text-5xl font-rubik text-secondary dark:text-primary">Timothie</div>
+    <div class="flex justify-between items-center p-8 lg:px-12 relative z-20">
+      <div class="text-3xl font-rubik text-auto">Timothie</div>
 
       <!-- Mobile Toggle Button -->
       <div class="md:hidden z-30">
         <button class="block focus:outline-none" @click="isMenuOpen = !isMenuOpen">
-          <span class="text-5xl md:text-primary text-white dark:text-white">
+          <span v-if="isMenuOpen" class="text-5xl md:text-primary text-secondary dark:text-primary">
             <Icon icon="material-symbols:close-rounded"></Icon>
           </span>
-          <span class="text-5xl md:text-primary text-white dark:text-white">
+          <span v-else class="text-5xl md:text-primary text-secondary dark:text-primary">
             <Icon icon="material-symbols:menu-rounded"></Icon>
           </span>
         </button>
@@ -18,21 +18,25 @@
       <!-- Navbar Link -->
       <nav
         :class="[
-          `fixed inset-0 z-20 flex flex-col items-center justify-center bg-primary md:relative md:bg-transparent md:flex md:justify-between md:flex-row ${
+          `fixed inset-0 z-20 flex flex-col items-center justify-center bg-accent md:relative md:bg-transparent md:flex md:justify-between md:flex-row ${
             isMenuOpen ? 'block' : 'hidden'
           }
                 `,
         ]"
       >
-        <ul class="flex flex-col items-center space-y-10 md:flex-row md:space-x-10 md:space-y-0">
+        <ul class="flex flex-col items-center space-y-5 md:flex-row md:space-x-5 md:space-y-0">
           <li v-for="item in Menu" :key="item.name">
-            <a :href="item.href" class="btn-nav">
+            <a
+              :href="item.href"
+              class="block transition ease-linear md:text-lg lg:text-xl font-bold md:text-secondary hover:text-accent dark:text-primary sm:dark:text-primary dark:hover:text-accent px-4 py-2 border-b-2 border-transparent hover:border-accent rounded-xl"
+              @click="scrollToSection(item.href)"
+            >
               {{ item.name }}
             </a>
           </li>
         </ul>
 
-        <button @click="toggleDarkMode" class="text-white z-10 hidden md:block">
+        <button @click="toggleDarkMode" class="text-white ml-20 z-10 hidden md:block">
           <div class="flex flex-col items-center">
             <!-- Show moon icon if dark mode is off, otherwise show sun icon -->
             <Icon
@@ -62,6 +66,13 @@ const Menu = ref([
   { name: 'About', href: '#about' },
 ])
 
+const scrollToSection = href => {
+  isMenuOpen.value = false
+  const section = document.querySelector(href)
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' })
+  }
+}
 const isDarkMode = ref(localStorage.getItem('theme') === 'dark')
 
 const toggleDarkMode = () => {
