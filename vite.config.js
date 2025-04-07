9d413d15ecb8ae4100e57fa7426cc24cb7901689
timothieecantcode <timothienguyen@gmail.com>
2025-04-07 16:53:18 +0930

    update

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
@@ -6,4 +7,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   base: '/Portfolio',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
