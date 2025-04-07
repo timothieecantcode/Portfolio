2886706a2be9e7e1ac33f5868c6676883536faf1
timothieecantcode <timothienguyen@gmail.com>
2025-04-07 16:57:00 +0930

    update

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), autoprefixer()],
   base: '/Portfolio',
   resolve: {
     alias: {
